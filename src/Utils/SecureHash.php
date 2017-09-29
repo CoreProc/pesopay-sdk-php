6dbe3b9315d487fbdd156e693a85cbb276afbded
Justin de Leon <justin.deleon@coreproc.ph>
2017-09-29 16:41:01 +0800

    Add SecureHash util class

---
+++ src/Utils/SecureHash.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: justin
+ * Date: 29/09/2017
+ * Time: 4:14 PM
+ */
+
+namespace Coreproc\PesoPay\Sdk\Utils;
+
+class SecureHash
+{
+    /**
+     * Verify the datafeed response.
+     *
+     * @note Included Merchant's secure hash secret in data param. Normally would access it from an config object or something.
+     * @param array $data Response data from PesoPay plus Merchant's secure hash secret
+     * @param string $hash Hash from PesoPay
+     * @return bool
+     */
+    public static function isValid(array $data, $hash)
+    {
+        $dataString = $data['src'] . '|'
+            . $data['prc'] . '|'
+            . $data['successcode'] . '|'
+            . $data['Ref'] . '|'
+            . $data['PayRef'] . '|'
+            . $data['Cur'] . '|'
+            . $data['Amt'] . '|'
+            . $data['payerAuth'] . '|'
+            . $data['secretCode'];
+
+        return sha1($dataString) === $hash;
+    }
+}
